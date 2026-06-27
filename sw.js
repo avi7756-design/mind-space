a2c4ceb3126e3dfb4196769c74557f6b89be6720
avi7756-design <avi7756@gmail.com>
2026-06-27 17:40:52 +0300

    ספריית מוזיקה אמיתית (CC0, בלי פרסומות) + דילוג + מועדפים + YouTube מוטמע בתוך האפליקציה (SW v4)

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-const C="mindspace-v3";
-const A=["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-192.png","./icon-512.png",
-  "./ambient-calm.mp3","./ambient-sleep.mp3","./ambient-focus.mp3","./ambient-energy.mp3"];
+const C="mindspace-v4";
+const A=["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting();});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
 self.addEventListener("fetch",e=>{
